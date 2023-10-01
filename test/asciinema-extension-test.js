cfda5e741a3ca4248e7f112f08e35f3e888d1296
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-10-01 09:04:05 +0100

    stash

---
+++ test/asciinema-extension-test.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const { expect } = require('./harness')
+const { name: packageName } = require('#package')
+
+describe('asciinema-extension', () => {
+  const ext = require(packageName + '/asciinema-extension')
+
+})
